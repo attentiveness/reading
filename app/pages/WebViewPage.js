7c2692fb1e022a3b5ae8f6d46e79c07e1a661efb
RichardCao <licheng.cao@ele.me>
2016-02-01 21:37:59 +0800

    用黑科技做了类似android弹窗的分享页面

--- app/pages/WebViewPage.js
+++ app/pages/WebViewPage.js
@@ -7,7 +7,9 @@ const {
   WebView,
   BackAndroid,
   Text,
+  Image,
   Platform,
+  TouchableOpacity,
   View
 } = React;
 
@@ -16,7 +18,9 @@ import {ToastShort} from '../utils/ToastUtils';
 import LoadingView from '../components/LoadingView';
 import {NaviGoBack} from '../utils/CommonUtils';
 import {shareToTimeline, shareToSession} from 'react-native-wechat';
+import Portal from 'react-native/Libraries/Portal/Portal.js';
 
+let tag;
 let toolbarActions = [
   {title: '分享', icon: require('../img/share.png'), show: 'always'}
 ];
@@ -30,11 +34,20 @@ class WebViewPage extends React.Component {
     this.onNavigationStateChange = this.onNavigationStateChange.bind(this);
   }
 
+  componentWillMount() {
+    if (Platform.OS === 'android') {
+      tag = Portal.allocateTag();
+    }
+  }
+
   componentDidMount() {
     const {navigator} = this.props;
     webviewRef = this.refs.webview;
     BackAndroid.addEventListener('hardwareBackPress', function() {
-      if (canGoBack) {
+      if (Portal.getOpenModals().length != 0) {
+        Portal.closeModal(tag);
+        return true;
+      } else if (canGoBack) {
         webviewRef.goBack();
         return true;
       }
@@ -43,23 +56,7 @@ class WebViewPage extends React.Component {
   }
 
   onActionSelected() {
-    // const {route} = this.props;
-    // shareToTimeline({
-    //   title: 'Reading:' + route.article.title,
-    //   thumbImage: route.article.contentImg,
-    //   type: 'news',
-    //   webpageUrl: route.article.url
-    // })
-    // .catch((error) => {ToastShort(error.message)});
-
-    // shareToSession({
-    //   title: 'Reading',
-    //   description: route.article.title,
-    //   thumbImage: route.article.contentImg,
-    //   type: 'news',
-    //   webpageUrl: route.article.url
-    // })
-    // .catch((error) => {ToastShort(error.message)});
+    Portal.showModal(tag, this.renderSpinner());
   }
 
   onNavigationStateChange(navState) {
@@ -68,6 +65,71 @@ class WebViewPage extends React.Component {
 
   renderLoading() {
     return <LoadingView />;
+  }
+
+  renderSpinner() {
+    const {route} = this.props;
+    return (
+      <View
+        key={'spinner'}
+        style={styles.spinner}
+      >
+        <View style={styles.spinnerContent}>
+          <Text style={[styles.spinnerTitle, {fontSize: 20, color: 'black'}]}>
+            分享到
+          </Text>
+          <View style={{flexDirection: 'row', marginTop: 20}}>
+            <TouchableOpacity
+              style={{flex: 1}}
+              onPress={() => {
+                shareToSession({
+                  title: 'Reading',
+                  description: route.article.title,
+                  thumbImage: route.article.contentImg,
+                  type: 'news',
+                  webpageUrl: route.article.url
+                })
+                .catch((error) => {
+                  ToastShort(error.message);
+                });
+            }}>
+              <View style={styles.shareContent}>
+                <Image
+                  style={styles.shareIcon}
+                  source={require('../img/share_icon_wechat.png')}
+                />
+                <Text style={styles.spinnerTitle}>
+                  微信
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{flex: 1}}
+              onPress={() => {
+                shareToTimeline({
+                  title: '[Reading]' + route.article.title,
+                  thumbImage: route.article.contentImg,
+                  type: 'news',
+                  webpageUrl: route.article.url
+                })
+                .catch((error) => {
+                  ToastShort(error.message);
+                });
+            }}>
+              <View style={styles.shareContent}>
+                <Image
+                  style={styles.shareIcon}
+                  source={require('../img/share_icon_moments.png')}
+                />
+                <Text style={styles.spinnerTitle}>
+                  朋友圈
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
   }
 
   render() {
@@ -102,6 +164,38 @@ let styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column'
+  },
+  spinner: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  spinnerContent: {
+    width: 200,
+    height: 150,
+    backgroundColor: '#fcfcfc',
+    padding: 20,
+    borderBottomColor: '#ddd',
+    borderLeftColor: '#ddd',
+    borderRightColor: '#ddd',
+    borderTopColor: '#ddd',
+    borderWidth: 2
+  },
+  spinnerTitle: {
+    fontSize: 18,
+    color: '#313131',
+    textAlign: 'center'
+  },
+  shareContent: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  shareIcon: {
+    width: 40,
+    height: 40
   }
 })
 
