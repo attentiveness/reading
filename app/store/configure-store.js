1ce68b24882d8147300d13eaa45b2f8cb33018d2
RichardCao <licheng.cao@ele.me>
2016-05-20 16:57:36 +0800

    add redux-logger and Log only in development

--- app/store/configure-store.js
+++ app/store/configure-store.js
@@ -2,11 +2,16 @@
 
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 import rootReducer from '../reducers/index';
 
-const logger = createLogger();
-const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  const createLogger = require('redux-logger');
+  const logger = createLogger();
+  middlewares.push(logger);
+}
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
 
 export default function configureStore(initialState) {
 	const store = createStoreWithMiddleware(rootReducer, initialState);
