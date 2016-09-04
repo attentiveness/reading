48d84177eb4ef6236096148c766cc5f7c61d4f6c
Zaicheng Qi <vmlinz@gmail.com>
2016-09-04 18:09:39 +0800

    Finish adding tests for read sagas

--- app/sagas/_spec_/category.spec.js
+++ app/sagas/_spec_/category.spec.js
@@ -1,6 +1,23 @@
+/**
+ *
+ * Copyright 2016-present reading
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 import { expect } from 'chai';
-
 import { put, call } from 'redux-saga/effects';
+
 import { requestTypeList } from '../category';
 import { fetchTypeList, receiveTypeList } from '../../actions/category';
 import { request } from '../../utils/RequestUtil';
@@ -25,7 +42,7 @@ describe('category saga tests', () => {
     },
   };
 
-  it('should dispatch fetchTypeList', () => {
+  it('should put(fetchTypeList())', () => {
     const next = step();
     expect(next).to.deep.equal(put(fetchTypeList()));
   });
