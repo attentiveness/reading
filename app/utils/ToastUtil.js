81efc8fc82d43897d5720bed1a64a5078a5f3550
Richard-Cao <403164405@qq.com>
2016-11-18 00:27:25 +0800

    fix toast

--- app/utils/ToastUtil.js
+++ app/utils/ToastUtil.js
@@ -16,51 +16,29 @@
  *
  */
 import {
-  Platform,
-  Alert
+  Alert,
+  ToastAndroid,
+  Platform
 } from 'react-native';
-import Toast from 'react-native-root-toast';
-
-let toast;
 
 export const toastShort = (content, isAlert) => {
-  if (toast !== undefined) {
-    Toast.hide(toast);
-  }
-  if (isAlert) {
+  if (isAlert || Platform.OS === 'ios') {
     Alert.alert(
       '提示',
       content.toString()
     );
   } else {
-    toast = Toast.show(content.toString(), {
-      duration: Toast.durations.SHORT,
-      position: Platform.OS === 'android' ? Toast.positions.BOTTOM : Toast.positions.CENTER,
-      shadow: true,
-      animation: true,
-      hideOnPress: true,
-      delay: 0
-    });
+    ToastAndroid.show(content.toString(), ToastAndroid.SHORT);
   }
 };
 
 export const toastLong = (content, isAlert) => {
-  if (toast !== undefined) {
-    Toast.hide(toast);
-  }
-  if (isAlert) {
+  if (isAlert || Platform.OS === 'ios') {
     Alert.alert(
       '提示',
       content.toString()
     );
   } else {
-    toast = Toast.show(content.toString(), {
-      duration: Toast.durations.LONG,
-      position: Platform.OS === 'android' ? Toast.positions.BOTTOM : Toast.positions.CENTER,
-      shadow: true,
-      animation: true,
-      hideOnPress: true,
-      delay: 0
-    });
+    ToastAndroid.show(content.toString(), ToastAndroid.LONG);
   }
 };
