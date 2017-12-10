0431174beac227230a889bdafc5cd4ead4eaec64
Richard-Cao <403164405@qq.com>
2017-12-11 00:41:15 +0800

    upgrade react-native to 0.51.0 and update app versions

--- ios/reading/AppDelegate.m
+++ ios/reading/AppDelegate.m
@@ -34,7 +34,6 @@
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
   
-  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
